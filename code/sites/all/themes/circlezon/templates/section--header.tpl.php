711fea5506d5b14b2273c34b45a428d49ca42d61
Shiraj <shiraj@askit.com>
2013-01-30 01:57:07 +0530

    Changes by Ambika on Card page grid layout

--- code/sites/all/themes/circlezon/templates/section--header.tpl.php
+++ code/sites/all/themes/circlezon/templates/section--header.tpl.php
@@ -1,3 +1,11 @@
 <header<?php print $attributes; ?>>
+<script type='text/javascript'>
+(function (d, t) {
+  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  bh.type = 'text/javascript';
+  bh.src = '//www.bugherd.com/sidebarv2.js?apikey=v3hnflbgcxkyxllgwgqlww';
+  s.parentNode.insertBefore(bh, s);
+  })(document, 'script');
+</script>
   <?php print $content; ?>
 </header>
