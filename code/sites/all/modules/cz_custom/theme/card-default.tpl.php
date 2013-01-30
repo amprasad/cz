3ca9c91552a09aa95cf744b8fe7281abe6c828aa
David Oliveros Zambrano <dato.oliveros@gmail.com>
2013-01-30 03:08:53 -0600

    Separated theme/templating functions and actual logic

---
+++ code/sites/all/modules/cz_custom/theme/card-default.tpl.php
@@ -0,0 +1,28 @@
+<div class="contact_grid_container">
+	<?php foreach ($data as $account) {
+		dpm($account);
+  	$pic =  $account->picture;
+  	if($pic == NULL || $pic == '') {
+    	$image = '<img src ="' . base_path() . 'sites/all/themes/circlezon/images/profile_placeholder.png'.'"/>'; 
+  	} else {
+      $image = '<img class = "contact_picture" src="'. $pic .'" />'; 
+    } ?>
+		<div class="contact_container">
+    	<div class="contact_data">
+    		<?php print $image ?>
+    	</div>
+      <div class="contact_data">
+      	<a href="<?php print $account->profileurl; ?>" target="_blank"><?php print $account->fullname; ?></a>
+      </div>
+     	<div class="contact_data contact_data_bold">
+     		<?php print $account->headline; ?>
+     	</div>
+      <div class="contact_data contact_data_bold">
+      	<?php $account->industry; ?>
+      </div>
+      <div class="contact_data">
+      	<?php $account->address; ?>
+      </div>
+    </div>
+  <?php } ?> 
+</div>
